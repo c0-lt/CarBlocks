33b3a3caef441e1c379b2405af71e1c9563ab030
Quentin Collette <quentin.collette@devoteamgcloud.com>
2022-12-04 09:36:14 +0100

    cleaning

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { EthProvider } from "./contexts/EthContext";
 import Intro from "./components/Intro/";
-import Setup from "./components/Setup";
 import Demo from "./components/Demo";
 import Footer from "./components/Footer";
 import "./App.css";
@@ -8,11 +7,9 @@ import "./App.css";
 function App() {
   return (
     <EthProvider>
-      <div id="App" >
+      <div id="App">
         <div className="container">
           <Intro />
-          <hr />
-          <Setup />
           <hr />
           <Demo />
           <hr />
